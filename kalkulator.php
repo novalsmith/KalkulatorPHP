97a9e6d4180a6a694489435b7d231e54e1199882
novalsmith <novlasmith69@gmail.com>
2018-03-28 04:08:41 -0700

    kalkulator v.01 Update

--- kalkulator.php
+++ kalkulator.php
@@ -1,3 +1,60 @@
+<!Doctype html>
+ <!--html5-->
+ <html>
+<head>
+<title> Program PHP Pertama Saya ! 
+</title>
+
+ <link rel="icon" type="image/png/jpg/gif/" href="icon.jpg" >
+
+</head>
+<body>
+	
 <?php 
-echo "Kalkulator";
+	/*
+ echo "Program  PHP  Pertama Saya <br> ";
+	 echo "PHP Teknik Malam <br> "; 
+	 echo "Belajar Pemrograman  PHP ";
+	*/
+
+// Variable PHP
+$nilaiPertama = 90;
+$nilaiKedua	  = 70;
+$hasil=0;
+
+// if($nilaiPertama+$nilaiKedua)
+// {
+
+// echo $hasil = $nilaiPertama + $nilaiKedua;
+// }
+//   operasi aritmatika  + - * / %
+echo "Penjumblahan <br>";
+echo $hasil = $nilaiPertama + $nilaiKedua;
+echo "<br>";
+echo "Pengurangan <br>";
+echo $hasil = $nilaiPertama - $nilaiKedua;
+echo "<br>";
+echo "Perkalian <br>";
+echo $hasil = $nilaiPertama * $nilaiKedua;
+echo "<br>";
+echo "Pembagian <br>";
+echo $hasil = $nilaiPertama / $nilaiKedua;
+echo "<br>";
+echo "Modulus <br>";
+echo $hasil = $nilaiPertama % $nilaiKedua;
+
  ?>
+	
+
+
+</body>
+ </html>
+
+
+
+
+
+
+
+
+
